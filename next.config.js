65a4839bd934f92b7c57083325ce2c7a863426ed
Viswanathnani12 <viswanathnani12@gmail.com>
2024-10-15 12:18:15 +0530

    edited next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-// next.config.js
+const isGithubPages = process.env.NODE_ENV === "production";
+
+const repoName = "PLacement_Plus"; // Replace with your GitHub repository name
+
 module.exports = {
-  output: "export", // This tells Next.js to output static files.
-  distDir: "out", // Set output folder to 'out' (default for static exports).
+  output: "export",
+  distDir: "out",
   images: {
-    unoptimized: true, // Disable Next.js image optimization for static export (useful for GitHub Pages).
+    unoptimized: true,
   },
-  trailingSlash: true, // Add a trailing slash to routes (optional, helps with routing in GitHub Pages).
-  assetPrefix: "./", // Add a relative path prefix for assets (important for GitHub Pages).
+  trailingSlash: true,
+  assetPrefix: isGithubPages ? `/${repoName}/` : "/",
 };
